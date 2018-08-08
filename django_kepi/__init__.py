6c3f76fb8f7999ce4322c0b328bc7c54df9904c2
Marnanel Thurman <marnanel@thurman.org.uk>
2018-08-08 19:41:05 +0100

    Add fields

---
+++ django_kepi/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'django_kepi'
+__version__ = '0.0.1'
+VERSION = __version__
+__author__ = 'Marnanel Thurman'
+__license__ = 'GPL-2'
+__copyright__ = 'Copyright (c) 2018 Marnanel Thurman'
+
