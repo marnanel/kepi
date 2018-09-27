20ff4d636d4140d00229fd82841f108a69f75323
Marnanel Thurman <marnanel@thurman.org.uk>
2018-09-27 16:38:41 +0100

    Tests for non-json messages

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
-Test for non-json messages
 Test for messages where all the parts are known
 Many more tests
 
--- tests/test_inbox.py
+++ tests/test_inbox.py
@@ -42,3 +42,51 @@ class TestInbox(TestCase):
         self.assertTrue(
                 QuarantinedMessage.objects.filter(username=None).exists())
 
+    def test_non_json(self):
+
+        QuarantinedMessage.objects.all().delete()
+
+        c = Client()
+
+        c.post('/sharedInbox',
+                content_type = 'text/plain',
+                data = 'Hello',
+                )
+
+        self.assertFalse(
+                QuarantinedMessage.objects.all().exists())
+
+    def test_malformed_json(self):
+
+        QuarantinedMessage.objects.all().delete()
+
+        c = Client()
+
+        text = """{
+                    "id": "https://example.net/hello-world",
+                    "actor": "https://users.example.net/mary",
+                    "object": "https://things.example.org/lamb",
+                    "type": "Like"
+                    }"""
+
+        c.post('/sharedInbox',
+                content_type = 'application/activity+json',
+                data = text,
+                )
+
+        self.assertTrue(
+                QuarantinedMessage.objects.all().exists())
+
+        QuarantinedMessage.objects.all().delete()
+
+        text = text[1:] # remove leading {, so the JSON is invalid
+
+        c.post('/sharedInbox',
+                content_type = 'application/activity+json',
+                data = text,
+                )
+
+        self.assertFalse(
+                QuarantinedMessage.objects.all().exists())
+
+
