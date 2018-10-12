45e1e405ef30ebc5aef3f1665e0ade9f86e920bc
Marnanel Thurman <marnanel@thurman.org.uk>
2018-10-12 16:25:46 +0100

    Using central logger. Improved log messages.

--- django_kepi/tasks.py
+++ django_kepi/tasks.py
@@ -1,9 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
+from django_kepi import logger
 import requests
-import logging
-
-logger = logging.getLogger('django_kepi.tasks')
 
 @shared_task()
 def fetch(
@@ -14,26 +12,20 @@ def fetch(
         ):
 
     if post_data is None:
-        logger.info('%s: GET', fetch_url)
+        logger.info('batch %s: GET', fetch_url)
 
         fetch = requests.get(fetch_url)
     else:
-        logger.info('%s: POST', url)
-        logger.debug('%s: with form data: %s',
+        logger.info('batch %s: POST with form data: %s',
                 fetch_url, post_data)
 
         fetch = requests.post(fetch_url,
                 data=post_data)
 
-    logger.info('%s: response code was %d',
-            fetch_url, fetch.status_code)
-    logger.debug('%s: body was: %s',
-            fetch_url, fetch.text)
+    logger.info('batch %s: response code was %d, body was %s',
+            fetch_url, fetch.status_code, fetch.text)
 
     if result_url is not None:
-        logger.info('%s: notifying %s',
-            fetch_url, result_url)
-
         response = requests.post(
                 result_url,
                 params={
@@ -44,8 +36,5 @@ def fetch(
                 )
 
         if response.status_code!=200:
-            logger.warn('%s: notifying %s FAILED with code %d',
+            logger.warn('batch %s: notifying %s FAILED with code %d',
                     fetch_url, result_url, response.status_code)
-        else:
-            logger.info('%s: notification received okay',
-                    fetch_url)
