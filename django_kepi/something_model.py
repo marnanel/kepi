d50333cc444f9a46536b082081c0b66d5d50facb
Marnanel Thurman <marnanel@thurman.org.uk>
2018-10-29 14:33:33 +0000

    fixme notes

--- django_kepi/something_model.py
+++ django_kepi/something_model.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django_kepi import object_type_registry
 from django_kepi.cache_model import Cache
-import json
 
 #######################
 
@@ -18,7 +17,13 @@ class SomethingManager(models.Manager):
 
         print(list(result))
 
+        # XXX Trouble: this returns everything as a Cache,
+        # whereas we want it to return a Person or whatever.
+        # XXX Make a list and pass the whole list to union() once.
+        # XXX Make a dummy first entry that returns zero Persons
+        # (or whatever) so it gets the type right.
         for local_handler in object_type_registry[self.f_type]:
+            print(local_handler)
             result = result.union(local_handler.objects.all())
             print(list(result))
 
