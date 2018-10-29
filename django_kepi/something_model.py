8317f82a43ea06e06e099cba1b24f71ed0fbf1de
Marnanel Thurman <marnanel@thurman.org.uk>
2018-10-29 14:39:52 +0000

    Person lookups return Persons correctly; they don't delegate to the right types yet

--- django_kepi/something_model.py
+++ django_kepi/something_model.py
@@ -9,25 +9,19 @@ class SomethingManager(models.Manager):
     f_type = None
 
     def get_queryset(self):
-        # XXX Make sure all QSs return url
         # TODO augment QSs with contenttypes link to source
-        result = Cache.objects.filter(
-                f_type=self.f_type,
+
+        result = models.QuerySet(
+                model = self.parent_model,
                 )
 
-        print(list(result))
+        handlers = [
+            Cache.objects.filter(f_type=self.f_type),
+            ]
 
-        # XXX Trouble: this returns everything as a Cache,
-        # whereas we want it to return a Person or whatever.
-        # XXX Make a list and pass the whole list to union() once.
-        # XXX Make a dummy first entry that returns zero Persons
-        # (or whatever) so it gets the type right.
-        for local_handler in object_type_registry[self.f_type]:
-            print(local_handler)
-            result = result.union(local_handler.objects.all())
-            print(list(result))
-
-        print('all')
+        handlers.extend([
+            h.objects.all()
+            for h in object_type_registry[self.f_type]])
 
         return result
 
@@ -41,3 +35,6 @@ class Person(models.Model):
 
     class Meta:
         managed = False
+
+Person.objects.parent_model = Person # this is ugly
+
