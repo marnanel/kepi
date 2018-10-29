9fcf27b4ee7b68695745bd7fadf53b7e8003df82
Marnanel Thurman <marnanel@thurman.org.uk>
2018-10-29 15:18:23 +0000

    Annotate queries with is_local and content type. Next step is using them.

--- django_kepi/something_model.py
+++ django_kepi/something_model.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django_kepi import object_type_registry
 from django_kepi.cache_model import Cache
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 #######################
 
@@ -9,21 +11,40 @@ class SomethingManager(models.Manager):
     f_type = None
 
     def get_queryset(self):
-        # TODO augment QSs with contenttypes link to source
 
+        # This is an empty QuerySet; it's here because
+        # it sets the type of the objects in the result.
         result = models.QuerySet(
                 model = self.parent_model,
                 )
 
+        cache_query = Cache.objects.filter(f_type=self.f_type)
+        cache_query.annotate(
+                is_local = models.Value(
+                    False
+                    ),
+                content_type = models.Value(
+                    ContentType.objects.get_for_model(Cache),
+                    ),
+                )
+
         handlers = [
-            Cache.objects.filter(f_type=self.f_type),
+                cache_query,
             ]
 
-        handlers.extend([
-            h.objects.all()
-            for h in object_type_registry[self.f_type]])
+        for h in object_type_registry[self.f_type]:
+            query = h.objects.all()
+            query.annotate(
+                    is_local = models.Value(
+                        True,
+                        ),
+                    content_type = models.Value(
+                        ContentType.objects.get_for_model(h),
+                        ),
+                    )
+            handlers.append(query)
 
-        return result
+        return result.union(*handlers)
 
 #######################
 
@@ -32,6 +53,16 @@ class Person(models.Model):
     f_type = 'Person'
     objects = SomethingManager()
     objects.f_type = f_type
+
+    content_type = models.ForeignKey(
+            ContentType,
+            on_delete=models.CASCADE,
+            ),
+
+    content_object = GenericForeignKey(
+            'content_type',
+            'url',
+            ),
 
     class Meta:
         managed = False
