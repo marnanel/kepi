3ab1531f50d09861118e5d2accd2601339773fa0
Marnanel Thurman <marnanel@thurman.org.uk>
2019-05-21 18:10:23 +0100

    URL paths for objects go to ThingView, not KepiView (which is the superclass)

--- django_kepi/urls.py
+++ django_kepi/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from django_kepi.views import *
 
 urlpatterns = [
-        path('<uuid:id>', KepiView.as_view()),
-        path('users/<str:name>', KepiView.as_view()),
+        path('<uuid:id>', ThingView.as_view()),
+        path('users/<str:name>', ThingView.as_view()),
         path('users/<str:name>/inbox', InboxView.as_view()),
         path('users/<str:name>/followers', FollowersView.as_view()),
         path('users/<str:name>/following', FollowingView.as_view()),
