35ae4ba5a8af1bcbec01e0279b2b7629a559e3ce
Marnanel Thurman <marnanel@thurman.org.uk>
2019-05-21 19:03:07 +0100

    Added request(), accept(), and reject().
    Also, Following.__str__() had the "pending" messages backwards.

--- django_kepi/models/following.py
+++ django_kepi/models/following.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django_kepi.models.thing import Thing
 import logging
 
 logger = logging.getLogger(name='django_kepi')
@@ -21,9 +20,9 @@ class Following(models.Model):
     def __str__(self):
 
         if self.pending:
-            pending = ''
-        else:
             pending = ' (pending acceptance)'
+        else:
+            pending = ''
 
         result = '[%s follows %s%s]' % (
                 self.follower,
@@ -31,5 +30,73 @@ class Following(models.Model):
                 pending,
                 )
         return result
+
+def _get_follow(follower, following):
+    try:
+        return Following.objects.get(follower=follower, following=following)
+    except Following.DoesNotExist:
+        return None
+
+def request(follower, following):
+
+    f = _get_follow(follower, following)
+
+    if f is not None:
+
+        logger.warn('follow request failed; %s already exists',
+            f)
+
+        if f.pending:
+            raise ValueError('%s has already requested to follow %s',
+                    follower, following)
+        else:
+            raise ValueError('%s is already following %s',
+                    follower, following)
+
+    result = Following(
+            follower = follower,
+            following = following,
+            pending = True,
+            )
+    result.save()
+
+    logger.info('%s has requested to follow %s',
+            follower, following)
+
+def accept(follower, following):
+
+    result = _get_follow(follower, following)
+
+    if result is None:
+
+        logger.warn('accepting follow request that we didn\'t know about')
+
+        result = Following(
+                follower = follower,
+                following = following,
+                pending = False,
+                )
+    else:
+        result.pending = False
+
+    result.save()
+
+    logger.info('%s has started to follow %s: %s',
+            follower, following, result)
+
+    return result
+
+def reject(follower, following):
+
+    f = _get_follow(follower, following)
+
+    if f is None:
+        logger.warn('rejecting follow request; '+
+            'that\'s fine because we didn\'t know about it')
+    else:
+        f.delete()
+
+    logger.info('%s was rejected as a follower of %s',
+            follower, following)
 
 
