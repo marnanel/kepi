52f3a504440ce9d5537c3e24c42f5191fb1cdc13
Marnanel Thurman <marnanel@thurman.org.uk>
2019-05-24 20:31:51 +0100

    validation goes back in django_kepi.validate

--- django_kepi/tasks.py
+++ django_kepi/tasks.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-from django_kepi.validation import IncomingMessage
 from django_kepi.find import find, find_local
 from django_kepi.models.thing import Thing
 from httpsig.verify import HeaderVerifier
@@ -89,78 +88,6 @@ def _signer_for_local_actor(local_actor):
             algorithm='rsa-sha256',
             headers=['(request-target)', 'host', 'date', 'content-type'],
             )
-
-class LocalDeliveryRequest(HttpRequest):
-
-    def __init__(self):
-        super().__init__()
-
-        self.method = 'POST'
-
-
-@shared_task()
-def validate(
-        message_id,
-        ):
-    logger.info('%s: begin validation',
-            message_id)
-
-    message = IncomingMessage.objects.get(id=message_id)
-
-    logger.debug('%s: received %s',
-            message_id, str(message))
-
-    actor = message.actor
-    key_id = message.key_id
-
-    logger.debug('%s: message signature is: %s',
-            message, message.signature)
-    logger.debug('%s: message body is: %s',
-            message, message.body)
-
-    actor_details = find(actor)
-
-    logger.debug('%s: actor details are: %s',
-            message, actor_details)
-
-    if actor_details is None:
-        logger.info('%s: actor %s does not exist; dropping message',
-            message, actor)
-        return None
-
-    # XXX key used to sign must "_obviously_belong_to" the actor
-
-    key = actor_details['publicKey']['publicKeyPem']
-
-    logger.debug('%s: public key is: %s',
-            message, key)
-
-    hv = HeaderVerifier(
-            headers = {
-                'Content-Type': message.content_type,
-                'Date': message.date,
-                'Signature': message.signature,
-                },
-            secret = key,
-            method = 'POST',
-            path = message.path,
-            host = message.host,
-            sign_header = 'Signature',
-        )
-
-    if not hv.verify():
-        logger.info('%s: spoofing attempt; message dropped',
-                message)
-        return None
-
-    logger.debug('%s: validation passed!', message)
-
-    result = Thing.create(
-            value=message.activity_form,
-            sender=actor,
-            )
-    logger.debug('%s: produced new Thing %s', message, result )
-    return result
 
 @shared_task()
 def deliver(
--- django_kepi/validation.py
+++ django_kepi/validation.py
@@ -1,11 +1,13 @@
 from django.db import models
+from celery import shared_task
 import logging
 import json
 import uuid
 import re
 from django.conf import settings
 from urllib.parse import urlparse
-from django_kepi import find
+import django_kepi.find
+import django_kepi.models.thing
 from httpsig.verify import HeaderVerifier
 
 logger = logging.getLogger(name='django_kepi')
@@ -102,12 +104,43 @@ class IncomingMessage(models.Model):
     def activity_form(self):
         return self.fields
 
-def is_local_user(url):
-    return urlparse(url).hostname in settings.ALLOWED_HOSTS
+@shared_task()
+def validate(
+        message_id,
+        ):
+    logger.info('%s: begin validation',
+            message_id)
 
-def _do_validation(message, key):
-    logger.debug('%s: running actual validation', message)
-    fields = message.fields
+    message = IncomingMessage.objects.get(id=message_id)
+
+    logger.debug('%s: received %s',
+            message_id, str(message))
+
+    actor = message.actor
+    key_id = message.key_id
+
+    logger.debug('%s: message signature is: %s',
+            message, message.signature)
+    logger.debug('%s: message body is: %s',
+            message, message.body)
+
+    actor_details = django_kepi.find.find(actor)
+
+    logger.debug('%s: actor details are: %s',
+            message, actor_details)
+
+    if actor_details is None:
+        logger.info('%s: actor %s does not exist; dropping message',
+            message, actor)
+        return None
+
+    # XXX key used to sign must "_obviously_belong_to" the actor
+
+    key = actor_details['publicKey']['publicKeyPem']
+
+    logger.debug('%s: public key is: %s',
+            message, key)
+
     hv = HeaderVerifier(
             headers = {
                 'Content-Type': message.content_type,
@@ -124,75 +157,15 @@ def _do_validation(message, key):
     if not hv.verify():
         logger.info('%s: spoofing attempt; message dropped',
                 message)
-        return
+        return None
 
-    logger.info('%s: validation passed...', message)
-    # XXX okay, go on, do something with it
+    logger.debug('%s: validation passed!', message)
 
-def validate(message,
-        second_pass=False):
+    result = django_kepi.models.thing.Thing.create(
+            value=message.activity_form,
+            sender=actor,
+            )
+    logger.debug('%s: produced new Thing %s', message, result )
+    return result
 
-    actor = message.actor
-    key_id = message.key_id
-
-    logger.debug('%s: begin validation; key_id is %s',
-            message, key_id)
-    logger.debug('%s: message signature is: %s',
-            message, message.signature)
-    logger.debug('%s: message body is: %s',
-            message, message.body)
-
-    if is_local_user(actor):
-        logger.debug('%s: actor %s is local', message, actor)
-
-        local_user = find(actor, 'Actor')
-
-        if local_user is None:
-            logger.info('%s: local actor %s does not exist; dropping message',
-                message, actor)
-            return
-
-        key = local_user.key
-        _do_validation(message, key)
-        return
-
-    if not _obviously_belongs_to(actor, key_id):
-        logger.info('%s: key_id %s is not obviously owned by '+\
-                'actor %s; dropping message',
-                message, key_id, actor)
-        return
-
-    try:
-        remote_key = CachedRemoteUser.objects.get(owner=actor)
-    except CachedRemoteUser.DoesNotExist:
-        remote_key = None
-
-    if remote_key is not None:
-
-        if remote_key.is_gone():
-            # XXX This should probably trigger a clean-out of everything
-            # we know about that user
-            logger.info('%s: remote actor %s is gone; dropping message',
-                    actor, message)
-            return
-
-        logger.debug('%s: we have the remote key', message)
-        _do_validation(message, remote_key.key)
-        return
-
-    logger.debug('%s: we don\'t have the key', message)
-
-    if second_pass:
-        logger.warning('%s: we apparently both do and don\'t have the key',
-                message)
-        return
-
-    message.waiting_for = actor
-    message.save()
-
-    if len(IncomingMessage.objects.filter(waiting_for=actor))==1:
-        logger.debug('%s: starting background task', message)
-        _kick_off_background_fetch(actor)
-    else:
-        logger.debug('%s: not starting background task', message)
 
