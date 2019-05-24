745ea992554c1858b0ab68c176ffb94b05435e54
Marnanel Thurman <marnanel@thurman.org.uk>
2019-05-24 20:22:34 +0100

    Remove admin for django_kepi (for now, anyway)

--- django_kepi/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from django_kepi import models
-
-admin.site.register(models.Thing)
--- test_settings.py
+++ test_settings.py
@@ -10,7 +10,6 @@ KEPI = {
 
 INSTALLED_APPS = (
 
-        'django.contrib.admin',
         'django.contrib.auth',
         'django.contrib.contenttypes',
         'django.contrib.sessions',
