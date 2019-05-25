5fd8c5f0db12f9239bbc0e946ede303647158c0a
Marnanel Thurman <marnanel@thurman.org.uk>
2019-05-25 18:42:09 +0100

    Anchored the regex

--- django_kepi/urls.py
+++ django_kepi/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from django_kepi.views import *
 
 urlpatterns = [
-        re_path('(?P<id>[0-9a-z]{8})', ThingView.as_view()),
+        re_path('^(?P<id>[0-9a-z]{8})$', ThingView.as_view()),
         path('users', AllUsersView.as_view()),
         path('users/<str:name>', ThingView.as_view()),
         path('users/<str:name>/inbox', InboxView.as_view()),
