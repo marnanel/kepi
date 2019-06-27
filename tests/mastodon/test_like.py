496aca9a83fc5c7345f5a44ef4108935d4454e5a
Marnanel Thurman <marnanel@thurman.org.uk>
2019-06-27 16:23:41 +0100

    test_like is pretty much working (and it fails)

--- tests/mastodon/test_like.py
+++ tests/mastodon/test_like.py
@@ -1,34 +1,28 @@
+from tests import create_local_note, create_local_person
+from django_kepi.models import create
 from django.test import TestCase
 
 class TestLike(TestCase):
 
     def test_like(self):
-        
 
-RSpec.describe ActivityPub::Activity::Like do
-  let(:sender)    { Fabricate(:account) }
-  let(:recipient) { Fabricate(:account) }
-  let(:status)    { Fabricate(:status, account: recipient) }
+        sender = create_local_person('sender')
+        recipient = create_local_person('recipient')
+        status = create_local_note(attributedTo=sender.id)
 
-  let(:json) do
-    {
-      '@context': 'https://www.w3.org/ns/activitystreams',
-      id: 'foo',
-      type: 'Like',
-      actor: ActivityPub::TagManager.instance.uri_for(sender),
-      object: ActivityPub::TagManager.instance.uri_for(status),
-    }.with_indifferent_access
-  end
+        object_json = {
+            '@context': 'https://www.w3.org/ns/activitystreams',
+            'id': 'foo',
+            'type': 'Like',
+            'actor': sender.id,
+            'object': status.id,
+          }
 
-  describe '#perform' do
-    subject { described_class.new(json, sender) }
+        # XXX What does Mastodon do with "with_indifferent_access" here?
 
-    before do
-      subject.perform
-    end
+        status = create(**object_json)
 
-    it 'creates a favourite from sender to status' do
-      expect(sender.favourited?(status)).to be true
-    end
-  end
-end
+        self.assertTrue(
+                sender.has_favourited(status),
+                msg = 'creates a favourite from sender to status',
+                )
