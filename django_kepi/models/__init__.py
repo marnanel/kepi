52b3491e669813681d4d4724f375ec091843af1a
Marnanel Thurman <marnanel@thurman.org.uk>
2019-07-03 21:00:36 +0100

    Export AUDIENCE_FIELD_NAMES

--- django_kepi/models/__init__.py
+++ django_kepi/models/__init__.py
@@ -1,6 +1,6 @@
 from .thing import Thing
 from .activity import Activity
 from .actor import Actor
-from .audience import Audience
+from .audience import Audience, AUDIENCE_FIELD_NAMES
 from .following import Following
 from .item import Item
