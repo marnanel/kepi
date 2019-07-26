9cad59bdc2ba0475b52411fb35b6691d6eef365a
Marnanel Thurman <marnanel@thurman.org.uk>
2019-07-26 17:52:34 +0100

    add fixme

--- django_kepi/models/activity.py
+++ django_kepi/models/activity.py
@@ -5,5 +5,10 @@ logger = logging.getLogger(name='django_kepi')
 
 ######################
 
+# FIXME The activity_form for Activities should
+# contain the whole activity_form in the 'object'
+# field, not just the id (which might not be
+# dereferencable anyway).
+
 class Activity(thing.Thing):
     pass
