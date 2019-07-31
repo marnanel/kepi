a3674c941d8b696dbf138e82d598a873cc0196ba
Marnanel Thurman <marnanel@thurman.org.uk>
2019-07-31 20:38:31 +0100

    Activity.go_into_outbox_if_local()

--- django_kepi/models/activity.py
+++ django_kepi/models/activity.py
@@ -11,4 +11,23 @@ logger = logging.getLogger(name='django_kepi')
 # dereferencable anyway).
 
 class Activity(thing.Thing):
-    pass
+
+    def go_into_outbox_if_local(self):
+
+        from django_kepi.models.collection import Collection
+        from django_kepi.find import find_local
+
+        if not self.is_local:
+            return
+
+        actor = self['actor__obj']
+
+        logger.info('%s: going into %s\'s outbox',
+                self.number,
+                actor.f_preferredUsername)
+
+        outbox_url = actor['outbox']
+        logger.debug('  -- which is %s', outbox_url)
+
+        outbox = find_local(outbox_url,
+                object_to_store=self)
