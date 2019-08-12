1f87fb44928560c7e3c43e8b20b563a3d75ec517
Marnanel Thurman <marnanel@thurman.org.uk>
2019-08-12 19:31:38 +0100

    Refactor create(). It could be more efficient, but let's look at that later.

--- django_kepi/create.py
+++ django_kepi/create.py
@@ -1,4 +1,5 @@
 from django_kepi.models import *
+import django.db.utils
 import logging
 
 logger = logging.getLogger(name='django_kepi')
@@ -13,7 +14,15 @@ def create(
     from django_kepi.delivery import deliver
 
     if value is None:
-        value = kwargs.copy()
+        value = {}
+
+    # Remove the "f_" prefix, which exists so that we can write
+    # things like f_type or f_object without using reserved keywords.
+    for k,v in kwargs.items():
+        if k.startswith('f_'):
+            value[k[2:]] = v
+        else:
+            value[k] = v
 
     logger.info("Create begins: source is %s; local? %s; run side effects? %s",
         value, is_local_user, run_side_effects)
@@ -22,26 +31,19 @@ def create(
         logger.warn("  -- it's ludicrous to create an Object with no value")
         return None
 
-    # Remove the "f_" prefix, which exists so that we can write
-    # things like f_type or f_object without using reserved keywords.
-    for k,v in kwargs.copy().items():
-        if k.startswith('f_'):
-            value[k[2:]] = v
-            del value[k]
-
     # Now, let's fix the types of keys and values.
-
-    for k,v in value.copy().items():
-        if not isinstance(k, str):
-            logger.warn('Objects can only have keys which are strings: %s',
-                    str(k))
-            continue
 
     if 'type' not in value:
         logger.warn("Objects must have a type; dropping message")
         return None
 
     value['type'] = value['type'].title()
+
+    for k,v in value.copy().items():
+        if not isinstance(k, str):
+            logger.warn('Objects can only have keys which are strings: %s',
+                    str(k))
+            del value[k]
 
     if 'id' in value:
         if is_local_user:
@@ -68,23 +70,51 @@ def create(
     logger.debug('Class for %s is %s', value['type'], cls)
     del value['type']
 
+    if 'url' and 'remote_url' in value:
+        if value['url']!=value['remote_url']:
+            logger.warn('url and remote_url differ (%s vs %s)',
+                    value['url'], value['remote_url'])
+
+        # in either case there's no point in keeping url around
+        del value['url']
+
     ########################
 
     # Right, we need to create an object.
 
-    result = cls()
-
     if 'id' in value:
-        result.remote_url = value['id']
-        del value['id']
+        try:
+            result = cls(
+                    remote_url = value['id'],
+                    )
+            del value['id']
+            result.save()
+        except django.db.utils.IntegrityError:
+            logger.warn('We already have an object with remote_url=%s',
+                value['id'])
+            return None
+    else:
+        result = cls(
+                )
+        result.save()
+
+    try:
+        result.save()
+    except Exception as e:
+        logger.warn('Couldn\'t save the new object (%s); dropping it',
+            e)
+        return None
 
     for f,v in value.items():
-        result[f] = v
+        try:
+            result[f] = v
+        except django.db.utils.Error as pe:
+            logger.warn('Can\'t set %s=%s on the new object (%s); bailing',
+                f, v, pe)
+            return None
 
     if hasattr(result, '_after_create'):
         result._after_create()
-
-    result.save()
 
     if run_side_effects:
         success = result.run_side_effects()
