cb551f50d9446ab38833057d1c019b8ae3375444
Marnanel Thurman <marnanel@thurman.org.uk>
2019-08-12 19:44:43 +0100

    test_validation largely rewritten so it uses httpretty. It passes.

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -2,11 +2,13 @@ from django.test import TestCase, Client
 from django_kepi.validation import IncomingMessage, validate
 from django_kepi.models import Object
 from unittest import skip
-from unittest.mock import Mock, patch
+import httpretty
 from . import *
 import logging
 import httpsig
 import json
+
+# FIXME This whole module should be rewritten to use httpretty
 
 # FIXME test caching
 # FIXME test invalid keys
@@ -15,7 +17,7 @@ logger = logging.getLogger(name='django_kepi')
 
 ACTIVITY_ID = "https://example.com/04b065f8-81c4-408e-bec3-9fb1f7c06408"
 INBOX_HOST = 'europa.example.com'
-INBOX_PATH = '/inbox'
+INBOX_PATH = '/sharedInbox'
 
 REMOTE_FRED = 'https://remote.example.org/users/fred'
 REMOTE_JIM = 'https://remote.example.org/users/jim'
@@ -24,62 +26,29 @@ FREDS_INBOX = REMOTE_FRED+'/inbox'
 JIMS_INBOX = REMOTE_JIM+'/inbox'
 REMOTE_SHARED_INBOX = 'https://remote.example.org/shared-inbox'
 
-LOCAL_ALICE = 'https://altair.example.com/users/alice'
-LOCAL_BOB = 'https://altair.example.com/users/bob'
-
-MESSAGE_CONTEXT = ["https://www.w3.org/ns/activitystreams",
-        "https://w3id.org/security/v1",
-        {"manuallyApprovesFollowers":"as:manuallyApprovesFollowers",
-            "sensitive":"as:sensitive",
-            "movedTo":{"@id":"as:movedTo",
-                "@type":"@id"},
-            "alsoKnownAs":{"@id":"as:alsoKnownAs",
-                "@type":"@id"},
-            "Hashtag":"as:Hashtag",
-            "ostatus":"http://ostatus.org#",
-            "atomUri":"ostatus:atomUri",
-            "inReplyToAtomUri":"ostatus:inReplyToAtomUri",
-            "conversation":"ostatus:conversation",
-            "toot":"http://joinmastodon.org/ns#",
-            "Emoji":"toot:Emoji",
-            "focalPoint":{"@container":"@list",
-                "@id":"toot:focalPoint"},
-            "featured":{"@id":"toot:featured",
-                "@type":"@id"},
-            "schema":"http://schema.org#",
-            "PropertyValue":"schema:PropertyValue",
-            "value":"schema:value"}]
-
-def _message_became_activity(url=ACTIVITY_ID):
-    try:
-        result = Object.objects.get(remote_url=url)
-        return True
-    except Object.DoesNotExist:
-        return False
-
-class ResultWrapper(object):
-    def __init__(self,
-            text='',
-            status_code=200,
-            ):
-        self.text = json.dumps(text)
-        self.status_code = status_code
+LOCAL_ALICE = 'https://testserver/users/alice'
+LOCAL_BOB = 'https://testserver/users/bob'
 
 class TestValidationTasks(TestCase):
 
-    @skip("Requires lookup of local actors' private keys, which requires delegate lookup")
-    @patch('requests.get')
-    def test_local_lookup(self, mock_get):
-        keys = json.load(open('tests/keys/keys-0000.json', 'r'))
+    def setUp(self):
+        settings.KEPI['LOCAL_OBJECT_HOSTNAME'] = 'testserver'
+
+    @httpretty.activate
+    def test_local_lookup(self):
+
+        keys = json.load(open('tests/keys/keys-0001.json', 'r'))
 
         alice = create_local_person(
                 name = 'alice',
-                f_publicKey = keys['public'],
+                publicKey = keys['public'],
                 )
-        alice.save()
-        logger.debug('%s', alice.url)
 
-        message = test_message(
+        bob = create_local_person(
+                name = 'bob',
+                )
+
+        body, headers = test_message_body_and_headers(
                 f_id=ACTIVITY_ID,
                 f_type="Follow",
                 f_actor=LOCAL_ALICE,
@@ -87,97 +56,171 @@ class TestValidationTasks(TestCase):
                 secret = keys['private'],
                 )
 
-        validate(message.id)
+        validate(path=INBOX_PATH,
+                headers=headers,
+                body=body,
+                is_local_user=False)
 
-        self.assertTrue(_message_became_activity())
-        mock_get.assert_not_called()
+        self.assertTrue(remote_object_is_recorded(ACTIVITY_ID),
+                msg="Message passed validation")
 
-    @patch('requests.get')
-    def test_remote_user_known(self, mock_get):
+    @httpretty.activate
+    def test_remote_user_known(self):
 
         keys = json.load(open('tests/keys/keys-0001.json', 'r'))
-        mock_get.return_value = ResultWrapper(
-                text = remote_user(
-                    url = REMOTE_FRED,
-                    name = 'Fred',
-                    # XXX This fails because publicKey is ordinarily a dict
-                    # containing the string, not just a string
-                    f_publicKey=keys['public']),
+        fetched = {
+                'fred': False,
+                }
+        def on_fetch():
+            fetched['fred'] = True
+
+        alice = create_local_person(
+                name = 'alice',
                 )
 
-        message = test_message(
+        create_remote_person(
+                url = REMOTE_FRED,
+                name = 'Fred',
+                publicKey=keys['public'],
+                on_fetch = on_fetch,
+                )
+
+        body, headers = test_message_body_and_headers(
                 f_id=ACTIVITY_ID,
                 f_type="Follow",
                 f_actor=REMOTE_FRED,
                 f_object=LOCAL_ALICE,
                 secret = keys['private'],
                 )
-        validate(message.id)
 
-        self.assertTrue(_message_became_activity())
-        mock_get.assert_called_once_with(REMOTE_FRED)
+        validate(path=INBOX_PATH,
+                headers=headers,
+                body=body,
+                is_local_user=False)
 
-    @patch('requests.get')
-    def test_remote_user_spoofed(self, mock_get):
+        self.assertTrue(remote_object_is_recorded(ACTIVITY_ID),
+                msg="Message passed validation")
+
+        self.assertTrue(
+                fetched['fred'],
+                msg="Fred's record was fetched from his server")
+
+    @httpretty.activate
+    def test_remote_user_spoofed(self):
+
         keys1 = json.load(open('tests/keys/keys-0001.json', 'r'))
         keys2 = json.load(open('tests/keys/keys-0002.json', 'r'))
-        mock_get.return_value = ResultWrapper(
-                text = remote_user(
-                    url = REMOTE_FRED,
-                    name = 'Fred',
-                    f_publicKey=keys2['public'],
-                ))
 
-        message = test_message(
+        fetched = {
+                'fred': False,
+                }
+
+        def on_fetch():
+            fetched['fred'] = True
+
+        create_remote_person(
+                url = REMOTE_FRED,
+                name = 'Fred',
+                publicKey=keys2['public'],
+                on_fetch = on_fetch,
+                )
+
+        body, headers = test_message_body_and_headers(
                 f_id=ACTIVITY_ID,
                 f_type="Follow",
                 f_actor=REMOTE_FRED,
                 f_object=LOCAL_ALICE,
                 secret = keys1['private'],
                 )
-        validate(message.id)
 
-        self.assertFalse(_message_became_activity())
+        logger.info('Test message headers: %s',
+                headers)
+        logger.info('Test message body: %s',
+                body)
 
-        mock_get.assert_called_once_with(REMOTE_FRED)
+        validate(path=INBOX_PATH,
+                headers=headers,
+                body=body,
+                is_local_user=False)
 
-    @patch('requests.get')
-    def test_remote_user_gone(self, mock_get):
+        self.assertFalse(remote_object_is_recorded(ACTIVITY_ID),
+                msg="Message failed validation")
+
+        self.assertTrue(
+                fetched['fred'],
+                msg="Fred's record was fetched from his server")
+
+    @httpretty.activate
+    def test_remote_user_gone(self):
+
         keys = json.load(open('tests/keys/keys-0001.json', 'r'))
-        mock_get.return_value = ResultWrapper(
-                status_code = 410,
+        fetched = {
+                'fred': False,
+                }
+        def on_fetch():
+            fetched['fred'] = True
+
+        mock_remote_object(
+                url = REMOTE_FRED,
+                content = "They went away",
+                status = 410,
+                on_fetch = on_fetch,
                 )
 
-        message = test_message(
+        body, headers = test_message_body_and_headers(
                 f_id=ACTIVITY_ID,
                 f_type="Follow",
                 f_actor=REMOTE_FRED,
                 f_object=LOCAL_ALICE,
                 secret = keys['private'],
                 )
-        validate(message.id)
 
-        self.assertFalse(_message_became_activity())
+        validate(path=INBOX_PATH,
+                headers=headers,
+                body=body,
+                is_local_user=False)
 
-        mock_get.assert_called_once_with(REMOTE_FRED)
+        self.assertFalse(remote_object_is_recorded(ACTIVITY_ID),
+                msg="Message failed validation")
 
-    @patch('requests.get')
-    def test_remote_user_unknown(self, mock_get):
+        self.assertTrue(
+                fetched['fred'],
+                msg="Fred's record was fetched from his server")
+ 
+    @httpretty.activate
+    def test_remote_user_unknown(self):
+
         keys = json.load(open('tests/keys/keys-0001.json', 'r'))
-        mock_get.return_value = ResultWrapper(
-                status_code = 404,
+        fetched = {
+                'fred': False,
+                }
+        def on_fetch():
+            fetched['fred'] = True
+
+        mock_remote_object(
+                url = REMOTE_FRED,
+                content = "They went away",
+                status = 404,
+                on_fetch = on_fetch,
                 )
 
-        message = test_message(
+        body, headers = test_message_body_and_headers(
                 f_id=ACTIVITY_ID,
                 f_type="Follow",
                 f_actor=REMOTE_FRED,
                 f_object=LOCAL_ALICE,
                 secret = keys['private'],
                 )
-        validate(message.id)
 
-        self.assertFalse(_message_became_activity())
+        validate(path=INBOX_PATH,
+                headers=headers,
+                body=body,
+                is_local_user=False)
 
-        mock_get.assert_called_once_with(REMOTE_FRED)
+        self.assertFalse(remote_object_is_recorded(ACTIVITY_ID),
+                msg="Message failed validation")
+
+        self.assertTrue(
+                fetched['fred'],
+                msg="Fred's record was fetched from his server")
 
