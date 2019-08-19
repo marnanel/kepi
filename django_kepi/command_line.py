8f3823ee6cd9c313daa54f2445e2c891bc53648c
Marnanel Thurman <marnanel@thurman.org.uk>
2019-08-19 17:37:13 +0100

    Commandline "kepi" script can now call manage.py commands beginning with "kepi-".
    Closes #5.

--- django_kepi/command_line.py
+++ django_kepi/command_line.py
@@ -4,25 +4,63 @@ from django.conf import settings
 from pkg_resources import resource_filename
 import django
 import os
+import sys
 
 KEPI_PREFIX = 'kepi-'
 
-def show_commands():
+def kepi_commands():
+    result = []
     for command in sorted(get_commands().keys()):
         if command.startswith(KEPI_PREFIX):
-            print(command[len(KEPI_PREFIX):])
+            result.append(command[len(KEPI_PREFIX):])
 
-def main():
+    return result
 
-    #resource_filename('kepi', 'settings.py'),
-    #
+def setup():
     os.environ.setdefault(
             "DJANGO_SETTINGS_MODULE",
             "kepi.settings"
             )
-
     django.setup()
 
-    show_commands()
+def show_help():
 
-    call_command('kepi-list')
+    progname = os.path.basename(sys.argv[0])
+
+    print()
+    print('Syntax: %s <command-name>' % (progname,))
+    print()
+    print('  where <command-name> is one of')
+
+    column = 0
+    width = 70
+    for command in kepi_commands():
+
+        if column+len(command)+1>width:
+            print('')
+            column = 0
+
+        if column==0:
+            print(' '*3, end='')
+            column += 3
+
+        print(' '+command, end='')
+        column += len(command)+1
+    
+    print()
+    print()
+    print('For more information on any command, use')
+    print('    %s <command-name> --help' % (progname,))
+    print()
+
+def main():
+    setup()
+
+    if len(sys.argv)<2 or sys.argv[1] in [
+            'help', '--help', '-h',
+            ]:
+        show_help()
+        return
+
+    call_command(KEPI_PREFIX + sys.argv[1],
+            *sys.argv[2:])
