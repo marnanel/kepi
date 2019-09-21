fad1aff083adf3d5f62a0ee75f98e900dd172a3b
Marnanel Thurman <marnanel@thurman.org.uk>
2019-09-21 17:09:30 +0100

    Media upload settings in settings

--- kepi/settings.py
+++ kepi/settings.py
@@ -131,6 +131,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 try:
     from .local_config import *
 except ModuleNotFoundError:
--- kepi/urls.py
+++ kepi/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.conf.urls.static import static
 import django_kepi.urls
+from . import settings
 
 urlpatterns = [
         path(r'admin/', admin.site.urls),
@@ -23,3 +25,5 @@ urlpatterns = [
         path(r'', include(django_kepi.urls)),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
