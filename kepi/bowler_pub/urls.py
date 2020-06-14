b3c761147434d00502cdce4c15cdd7dec6fcd7a3
Marnanel Thurman <marnanel@thurman.org.uk>
2020-06-14 22:12:43 +0100

    Remove ThingView and (for now) ActorView. InboxView and OutboxView don't get told their listnames (because they know what they are already).

--- kepi/bowler_pub/urls.py
+++ kepi/bowler_pub/urls.py
@@ -12,13 +12,10 @@ from django.urls import path, re_path
 import kepi.bowler_pub.views as bowler_pub_views
 
 urlpatterns = [
-        re_path('^(?P<id>[0-9a-z]{8})$', bowler_pub_views.ThingView.as_view()),
         path('users', bowler_pub_views.AllUsersView.as_view()),
-        path('users/<str:username>', bowler_pub_views.ActorView.as_view()),
-        path('users/<str:username>/inbox', bowler_pub_views.InboxView.as_view(),
-            { 'listname': 'inbox', } ),
-        path('users/<str:username>/outbox', bowler_pub_views.OutboxView.as_view(),
-            { 'listname': 'outbox', } ),
+        #path('users/<str:username>', bowler_pub_views.ActorView.as_view()),
+        path('users/<str:username>/inbox', bowler_pub_views.InboxView.as_view()),
+        path('users/<str:username>/outbox', bowler_pub_views.OutboxView.as_view()),
         path('users/<str:username>/followers', bowler_pub_views.FollowersView.as_view()),
         path('users/<str:username>/following', bowler_pub_views.FollowingView.as_view()),
         path('sharedInbox', bowler_pub_views.InboxView.as_view()),
