87c2f3444783e633699241232d468b26e9a3fd80
Marnanel Thurman <marnanel@thurman.org.uk>
2020-07-03 19:26:00 +0100

    k.bowler_pub.views exports PersonView

--- kepi/bowler_pub/views/__init__.py
+++ kepi/bowler_pub/views/__init__.py
@@ -1,11 +1,13 @@
 from .activitypub import \
         FollowersView, FollowingView, \
+        PersonView, \
         AllUsersView, \
         InboxView, OutboxView, \
         CollectionView
 
 __all__ = [
         'FollowersView', 'FollowingView',
+        'PersonView',
         'AllUsersView',
         'InboxView', 'OutboxView',
         'CollectionView',
