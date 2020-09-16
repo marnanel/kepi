c1368cd128916a088ffda8fd34e4894cf1427cab
Marnanel Thurman <marnanel@thurman.org.uk>
2020-09-16 23:18:25 +0100

    sombrero's test_delivery has assertions in. (The previous stub version didn't.)
    The tests for remote delivery are still unimplemented. Soon.

--- kepi/sombrero_sendpub/tests/test_delivery.py
+++ kepi/sombrero_sendpub/tests/test_delivery.py
@@ -10,63 +10,79 @@ logger = logging.getLogger(name='kepi')
 from unittest import skip
 from django.test import TestCase
 from kepi.sombrero_sendpub.delivery import deliver
-from kepi.bowler_pub.tests import create_local_person
+from kepi.trilby_api.tests import create_local_person
 from kepi.trilby_api.models import Follow
+from unittest.mock import MagicMock
+import kepi.bowler_pub.views as bowler_views
 import json
 
 TEST_ACTIVITY = {"hello": "world"}
 
 class TestDelivery(TestCase):
 
+    def setup_locals(self):
+        self.alice = create_local_person("alice")
+        self.bob = create_local_person("bob")
+        self.carol = create_local_person("carol")
+
+        self._real_inbox_view = bowler_views.InboxView.post
+        self._local_mock = MagicMock(
+                wraps=self._real_inbox_view,
+                )
+        bowler_views.InboxView.post = self._local_mock
+
     def test_send_to_local_users(self):
 
-        alice = create_local_person("alice")
-        bob = create_local_person("bob")
-        carol = create_local_person("carol")
+        self.setup_locals()
 
         deliver(
                 activity = TEST_ACTIVITY,
-                sender = alice,
+                sender = self.alice,
                 target_people = [
-                    bob, carol,
+                    self.bob, self.carol,
                     ],
                 )
+
+        self._local_mock.assert_called_once()
 
     def test_send_to_local_users_not_self(self):
 
-        alice = create_local_person("alice")
-        bob = create_local_person("bob")
-        carol = create_local_person("carol")
+        self.setup_locals()
 
         deliver(
                 activity = TEST_ACTIVITY,
-                sender = alice,
+                sender = self.alice,
                 target_people = [
-                    alice,
-                    bob, carol,
+                    self.alice,
+                    self.bob, self.carol,
                     ],
                 )
+
+        self._local_mock.assert_called_once()
 
     def test_send_to_followers_of_local_user(self):
-        alice = create_local_person("alice")
-        bob = create_local_person("bob")
-        carol = create_local_person("carol")
 
-        Follow(following=alice, follower=bob).save()
-        Follow(following=alice, follower=carol).save()
+        self.setup_locals()
+
+        Follow(following=self.alice, follower=self.bob).save()
+        Follow(following=self.alice, follower=self.carol).save()
 
         deliver(
                 activity = TEST_ACTIVITY,
-                sender = alice,
+                sender = self.alice,
                 target_followers_of = [
-                    alice,
+                    self.alice,
                     ],
                 )
+
+        self._local_mock.assert_called_once()
 
     @skip(reason="nyi")
     def test_send_to_remote_user(self):
+        # FIXME
         pass
 
     @skip(reason="nyi")
     def test_send_to_followers_of_remote_user(self):
+        # FIXME
         pass
