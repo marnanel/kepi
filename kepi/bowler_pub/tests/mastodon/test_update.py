972377aafac00d4fd5a1e67d5da19e196ece9452
Marnanel Thurman <marnanel@thurman.org.uk>
2020-10-27 20:40:28 +0000

    Updated test_update(), though it doesn't pass

--- kepi/bowler_pub/tests/mastodon/test_update.py
+++ kepi/bowler_pub/tests/mastodon/test_update.py
@@ -1,33 +1,45 @@
 from django.test import TestCase
-from kepi.trilby_api.tests import create_local_person
+from kepi.bowler_pub.tests import create_remote_person
 from unittest import skip
 from kepi.bowler_pub.create import create
 from kepi.bowler_pub.models import *
+import httpretty
 import logging
 
-SENDER_ID = 'https://example.com/actor'
-SENDER_DOMAIN = 'example.com'
-SENDER_FOLLOWERS = 'https://example.com/followers'
-
 logger = logging.getLogger(name='kepi')
+
+SENDER_ID = 'https://example.com/actor'
+TOTALLY = "Totally modified now"
 
 # XXX Why does this only test updating of profiles?
 # XXX I thought we should update items as well.
 
 class TestUpdate(TestCase):
 
+    @httpretty.activate
     def test_update_profile(self):
 
-        sender = create_local_person()
+        sender = create_remote_person(
+                name = 'jeremy',
+                remote_url = SENDER_ID,
+                auto_fetch = True,
+                )
 
-        object_json = {
+        create_form = {
                 '@context': 'https://www.w3.org/ns/activitystreams',
                 'id': 'foo',
                 'type': 'Update',
-                'actor': sender,
-                'object': actor_json,
+                'actor': SENDER_ID,
+                'object': {
+                    'display_name': TOTALLY,
+                    },
                 }
+
+        logger.info('Submitting Update activity: %s', create_form)
+        create(create_form)
+
         self.assertEqual(
                 sender.display_name,
-                "Totally modified now",
+                TOTALLY,
+                msg = "it updates profile",
                 )
