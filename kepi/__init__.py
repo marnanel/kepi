4a046f6a14330b77d7308f77f3f65594c9982d13
Marnanel Thurman <marnanel@thurman.org.uk>
2023-09-01 16:06:26 +0100

    copyright date bump

--- kepi/__init__.py
+++ kepi/__init__.py
@@ -3,4 +3,4 @@ __version__ = '0.7' # shine
 VERSION = __version__
 __author__ = 'Marnanel Thurman'
 __license__ = 'GPL-2'
-__copyright__ = 'Copyright (c) 2018-2020 Marnanel Thurman'
+__copyright__ = 'Copyright (c) 2018-2023 Marnanel Thurman'
