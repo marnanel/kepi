e3a9a22e7283d5c5feb2263f71ca520ad2606cd1
Marnanel Thurman <marnanel@thurman.org.uk>
2023-09-15 19:33:54 +0100

    first draft of Message

--- kepi/daemon/message.py
+++ kepi/daemon/message.py
@@ -1,2 +1,13 @@
-class Message():
+import mailbox
+
+class Store(mailbox.mbox):
     pass
+
+class Message(mailbox.mboxMessage):
+    def __init__(self,
+                 **kwargs):
+        for f,v in kwargs.items():
+            if f=='body':
+                self.set_payload(v)
+            else:
+                self[f] = v
